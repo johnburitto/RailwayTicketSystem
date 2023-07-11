20fccd00c2f2cf4d35e4d09b698ca85f2f1f31ee
Андрій Борсук <johnburitto@gmail.com>
2023-07-11 22:03:16 +0300

    Some fix

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddSwaggerGen();
 var smtpClient = new SmtpClient("smtp.gmail.com")
 {
     Port = 587,
-    Credentials = new NetworkCredential("borsuk.andrii@chnu.edu.ua", "10utezez"),
+    Credentials = new NetworkCredential("borsuk.andrii@chnu.edu.ua", "***********"),
     EnableSsl = true,
 };
 
